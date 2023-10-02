600e1e6fd29dd12f46388ec795624c25c03e6b6a
Sunny Hundal <amandip.h@gmail.com>
2023-10-02 15:55:29 -0700

    Initial commit + display a photo (using props)

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,19 +3,40 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
-  /* Insert React */
+
+
+const PhotoListItem = (props) => {
+  const {id, location, imageSource, username, profile} = props.sampleData;
+  
+  return (
+    <div className="photo-list-item__item" key = {id}>
+      <div className="photo-list-item__image-container">
+        <img
+          className="photo-list-item__image"
+          src={imageSource}
+          alt="A photo"
+        />
+      </div>
+      <div className="photo-list-item__user-details">
+        <div className="photo-list-item__location">
+          {location.city},{" "}
+          {location.country}
+        </div>
+        <div className="photo-list-item__username">
+          {username}
+        </div>
+        <div className="photo-list-item__profile">
+          <img
+            className="photo-list-item__profile-image"
+            src={profile}
+            alt="Profile"
+          />
+        </div>
+      </div>
+    </div>
+  );
+
 };
 
 export default PhotoListItem;
